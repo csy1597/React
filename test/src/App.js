6e6adb97a50efc7f36764cbe55cd759e8d575f3d
csy1597 <qlseo3@naver.com>
2021-10-13 01:30:14 +0900

    test2

--- test/src/App.js
+++ test/src/App.js
@@ -9,12 +9,21 @@ function App() {
     const [value, setValue] = useState('');
     // Event
     const onChange = (e) => setValue(e.target.value);
+    const onClick = () => {
+        console.log(value);
+        if(!value){
+            alert('아무것도 없다 이것아')
+        }
+    }
     // JSX
 
 
   return (
     <div className="App">
         <h1>ToDoList</h1>
+        <hr/>
+        <input value={value} onChange={onChange} />
+        <button onClick={onClick}>확인</button>
     </div>
   );
 }
