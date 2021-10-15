316144f601c5c4901ee8700a2c9eb2ddc257e956
csy1597 <qlseo3@naver.com>
2021-10-15 22:16:53 +0900

    test

--- test/src/App.js
+++ test/src/App.js
@@ -8,7 +8,14 @@ function App() {
     // State
     const [value, setValue] = useState('');
     // Event
-    
+    const onChange = (e) => setValue(e.target.value);
+    const onClick = () => {
+      console.log(value);
+      if(!value){
+        console.log('아무것도 입력이 되지 않았습니다.')
+      }
+      
+    }
     // JSX
 
 
@@ -16,7 +23,7 @@ function App() {
     <div className="App">
       <h1>게시판</h1>
       <input value={value} onChange={onChange} />
-        
+      <button value={value} onClick={onClick} />
     </div>
   );
 }
