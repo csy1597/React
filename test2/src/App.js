a80b535141a03fe59851460142819a36da558d59
csy1597 <qlseo3@naver.com>
2021-10-17 21:22:36 +0900

    reset

--- test2/src/App.js
+++ test2/src/App.js
@@ -6,59 +6,11 @@ import { useState } from 'react';
 function App() {
   
   //State
-  const [value, setValue] = useState('');
-  const [list, setList] = useState([]);
   //Event
-  const onChange = (e) => setValue(e.target.value);
-  const onClick = () => {
-    console.log(value);
-    if(!value){
-      alert('아무것도 없어요 !!!');
-      return;
-    }
-    setList((prevState) => [...prevState, value]);
-    setValue('');
-  };
-  const onEdit = (index) =>{
-    console.log(index);
-
-    const editText = prompt();
-    console.log(editText);
-    if(!editText){
-      alert('다시 입력 해주세요!');
-      return;
-    };
-
-    setList((prevState)=> {
-      const editList = prevState.map((item, i) =>{
-        return i === index ? editText:item;
-      });
-      return editList;
-    });
-  };
-  const onDelete =(index) =>{
-    console.log(index);
-
-    setList((prevState)=>{
-      const deleteList = prevState.filter((item, i)=>{
-        return i !== item&&index;
-      });
-      return deleteList;
-    });
-  }
+  
   // JSX
   return (
     <div className="App">
-      <h1> todo list </h1>
-      <input value={value} onChange={onChange} />
-      <button value={value} onClick={onClick}>Click</button>
-      {list.map((item, index) =>( 
-        <div key={index}>
-          <input value={item} readOnly/>
-          <button onClick= { () => onEdit(index)} >Edit</button>
-          <button onClick= { () => onDelete(index)} >Delete</button>
-        </div>
-      ))}
     </div>
   );
 }
