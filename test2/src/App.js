2aada4bc90c280bace9560afe39af3088cb6635e
csy1597 <qlseo3@naver.com>
2021-10-17 20:26:18 +0900

    onchange, onclick

--- test2/src/App.js
+++ test2/src/App.js
@@ -6,11 +6,22 @@ import { useState } from 'react';
 function App() {
   
   //State
+  const [value, setValue] = useState('');
   //Event
-
+  const onChange = (e) => setValue(e.target.value);
+  const onClick = () => {
+    console.log(value);
+    if(!value){
+      alert('아무것도 없어요 !!!');
+      return;
+    }
+  }
   // JSX
   return (
     <div className="App">
+      <h1> todo list </h1>
+      <input value={value} onChange={onChange} />
+      <button value={value} onClick={onClick} />
     </div>
   );
 }
