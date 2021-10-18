291247e09025d9f93a89b2438823e4efb9ca0948
csy1597 <qlseo3@naver.com>
2021-10-18 23:10:49 +0900

    list

--- test2/src/App.js
+++ test2/src/App.js
@@ -7,12 +7,20 @@ function App() {
   
   //State
   const [value, setValue] = useState('');
+  const [list, setList] = useState([]);
 
   //Event
   const onChange = (e) => setValue(e.target.value);
   const onClick = (value) => {
     console.log(value);
+    if(!value){
+      alert('아무것도 없습니다.!');
+      return;
+    };
+    setList((prevState) => [...prevState, value]);
+    setValue('');
   }
+  
 
 
   
@@ -22,6 +30,12 @@ function App() {
       <h1>TodoList</h1>
       <input value={value} onChange={onChange} />
       <button value={value} onClick={onClick}>Click</button>
+      {list.map((item, index) =>(
+        <div>
+          <input key={item} readOnly />
+          
+        </div>
+      ))}
     
     </div>
   );
