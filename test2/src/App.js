bf2856873ea3cc681a7f0c58cb63268d93f3dc64
csy1597 <qlseo3@naver.com>
2021-10-24 14:42:56 +0900

    reset

--- test2/src/App.js
+++ test2/src/App.js
@@ -16,7 +16,6 @@ function App() {
   const [userPw, setUserPw] = useState('');
   const [selected, setSelected] = useState('');
   const selectList = ['------','Apple', 'Dell' ,'SamSung', 'LG', 'HP'];
-  const [textArea, setTextArea] = useState('');
 
 
 
@@ -94,11 +93,6 @@ function App() {
     console.log(selected);
     return;
   }
-  const onTextArea = (e) => setTextArea(e.target.value);
-  console.log(textArea);
-  const textReset = () =>{
-    setTextArea('');
-  }
 
 
   
@@ -156,12 +150,6 @@ function App() {
           </option>
         ))}
       </select>
-      <p>Text Area</p>
-      <form onSubmit>
-        <textarea type="text" value={textArea} onChange={onTextArea} />
-        <button onClick>Submit</button>
-        <button type="reset" onClick={textReset}>Reset</button>
-      </form>
 
     
     </div>
