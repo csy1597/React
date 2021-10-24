f1c9f83ab063f2d8c88b6230db5e7399f6087075
csy1597 <qlseo3@naver.com>
2021-10-24 16:42:38 +0900

    textarea

--- test2/src/App.js
+++ test2/src/App.js
@@ -16,6 +16,8 @@ function App() {
   const [userPw, setUserPw] = useState('');
   const [selected, setSelected] = useState('');
   const selectList = ['------','Apple', 'Dell' ,'SamSung', 'LG', 'HP'];
+  const [textArea, setTextArea] = useState('');
+
 
 
 
@@ -93,6 +95,12 @@ function App() {
     console.log(selected);
     return;
   }
+  const onTextArea = (e) => setTextArea(e.target.value);
+  console.log(textArea);
+  const textReset = () =>{
+    setTextArea('');
+  }
+  
 
 
   
@@ -150,6 +158,13 @@ function App() {
           </option>
         ))}
       </select>
+      <p>TextArea</p>
+      <form onSubmit={onSubClick}>
+        <textarea type="text" value={textArea} onChange={onTextArea} />
+        <button onClick={onTextArea}>Submit</button>
+        <button type="reset" onClick={textReset}>Reset</button>
+
+      </form>
 
     
     </div>
