31e3c12ea36b2f51ca13e860640acfe3e0e002c9
csy1597 <qlseo3@naver.com>
2021-11-02 01:15:40 +0900

    reset

--- test2/src/App.js
+++ test2/src/App.js
@@ -24,8 +24,6 @@ function App() {
   const [click, setClick] = useState('');
   const [userName, setUserName] = useState('');
   const [inputNumber, setInputNumber] = useState('');
-  const [callBook, setCallBook] = useState('');
-  const [callBookList, setCallBookList] = useState([]);
   
 
 
@@ -145,42 +143,6 @@ function App() {
   const onSubmit = (e) => {
     e.preventDefault();
     console.log(userName, inputNumber);
-  };
-  const onCallBook = (e) => {
-    console.log(callBook);
-    const callBookregex = /^[0-9\b -]{0,11}$/
-    if(callBookregex.test(e.target.value)){
-      setCallBook(e.target.value);
-    }
-  };
-  const addCallBook = () => {
-    console.log(callBook);
-    if(!callBook){
-      alert("put your phone number");
-      return;
-    }
-    setCallBookList((prevState)=> [...prevState, callBook]);
-    setCallBook('');
-  }
-  const numberEdit = (index) => {
-    console.log(value);
-    const callEdit = prompt();
-    console.log(callEdit);
-    setCallBookList((prevState)=>{
-      const callEditList = prevState.map((item, i) =>{
-        return i === index ? callEdit:item;
-      });
-      return callEditList;
-    });
-  };
-  const numberDelete = (index) => {
-    console.log(index);
-    setCallBookList((prevState) => {
-      const callDeleteList = prevState.filter((item, i) => {
-        return i !== index && item;
-      });
-      return callDeleteList;
-    });
   };
 
   
@@ -312,22 +274,8 @@ function App() {
         <button type="submit">가입</button>
       </form>
       <hr/>
-      <p>전화번호부</p>
-      <input type="text" value={callBook} onChange={onCallBook} />
-      <button onClick={addCallBook}> ADD </button>
-      {callBookList.map((item, index) => (
-        <div key={index}>
-          <input value={item} readOnly />
-          <button onClick={() => numberEdit(index)}>Edit</button>
-          <button onClick={() => numberDelete(index)}>Delete</button>
-        </div>
-      ))}
-
-
 
       
-      
-
     </div>
   );
 }
